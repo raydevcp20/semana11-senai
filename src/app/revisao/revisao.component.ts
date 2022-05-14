591d3d83b887bf5c420781dded327d821a55d853
Rayane Cristina <68652767+raydevcp20@users.noreply.github.com>
2022-05-14 02:38:18 -0300

    Revisão aula 05

--- src/app/revisao/revisao.component.ts
+++ src/app/revisao/revisao.component.ts
@@ -15,6 +15,48 @@ export class RevisaoComponent implements OnInit {
   notEmail: boolean = false;
   regex: RegExp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 
+  users: any = [
+    {
+      primeiroNome: "Ray",
+      ultimo:"Silva"
+    },
+    {
+      primeiroNome: "João",
+      ultimo:"Fernando"
+    },
+    {
+      primeiroNome: "Raquel",
+      ultimo:"Oliveira"
+    },
+    {
+      primeiroNome: "Tulio",
+      ultimo:"Bastos"
+    }
+  ]
+  primeiroNome="";
+  segundoNome="";
+
+  indexAtual: number =0;
+  editandoUser: boolean = false;
+
+  getUser(index: number){
+    console.log(index)
+    this.users.splice(index, 1)
+  }
+
+  editUser(user:any, index:number){
+    this.primeiroNome = user.primeiroNome;
+    this.segundoNome = user.ultimo;
+    this.indexAtual = index;
+    this.editandoUser = true;
+  }
+
+  editingUser(){
+    this.users[this.indexAtual].primeiroNome = this.primeiroNome;
+    this.users[this.indexAtual].ultimo = this.segundoNome;
+    this.editandoUser = false;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
@@ -33,5 +75,9 @@ export class RevisaoComponent implements OnInit {
       console.log("aceito");
     }
   }
+
+
+
+
 
 }
